0847ff71906ac3bcffd908723e1f50c765e773d0
ivanzamaraev1 <ivanzamarave@gmail.com>
2021-04-01 16:13:01 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Suspense} from 'react'
+import { Posts } from './Posts'
+import { useResource } from './resource'
 
-function App() {
+
+export default function App() {
+  const resource = useResource()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className="container">
+      <h1>Suspense for Data Fetching</h1>
 
-export default App;
+      <Suspense fallback={<p>Loading</p>}>
+        <Posts resourse={resource}/>
+      </Suspense>
+      
+    </div>
+  )
+}
